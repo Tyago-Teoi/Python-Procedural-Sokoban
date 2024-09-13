fbdfed2e9f4e4592e24a9add7a695cd2bbf88c03
Tyago Teoi <tyago.teoi@usp.br>
2024-09-13 18:20:32 -0300

    Pass code and remove old script

--- sokoban.py
+++ sokoban.py
@@ -1,243 +1,135 @@
-"""
-Platformer Template
-"""
 import arcade
+import scripts.sokoplayer
 
-# --- Constants
-SCREEN_TITLE = "Sokoban"
+# Define constants for the screen
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Sokoban Level"
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
+# Define the size of each sprite
+SPRITE_SIZE = 60
+PLAYER_MOVEMENT_SPEED = 60
 
-# Constants used to scale our sprites from their original size
-CHARACTER_SCALING = 1
-TILE_SCALING = 0.5
-COIN_SCALING = 0.5
-SPRITE_PIXEL_SIZE = 128
-GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
+player_position_x = 0
+player_position_y = 0
 
-# Movement speed of player, in pixels per frame
-PLAYER_MOVEMENT_SPEED = 10
-GRAVITY = 1
-PLAYER_JUMP_SPEED = 20
+# Define the level matrix
 
-input_string = """
-----#####----------
-----#---#----------
-----#$--#----------
---###--$##---------
---#--$-$-#---------
-###-#-##-#---######
-#---#-##-#####--..#
-#-$--$----------..#
-#####-###-#@##--..#
-----#-----#########
-----#######--------
-"""
+level = [
+    ['-', '-', '-', '-', '#', '#', '#', '#', '#', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '#', '-', '-', '-', '#', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '#', '$', '-', '-', '#', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '#', '#', '#', '-', '-', '$', '#', '#', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '#', '-', '-', '$', '-', '$', '-', '#', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['#', '#', '#', '-', '#', '-', '#', '#', '-', '#', '-', '-', '-', '#', '#', '#', '#', '#', '#'],
+    ['#', '-', '-', '-', '#', '-', '#', '#', '-', '#', '#', '#', '#', '#', '-', '-', '.', '.', '#'],
+    ['#', '-', '$', '-', '-', '$', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '.', '.', '#'],
+    ['#', '#', '#', '#', '#', '-', '#', '#', '#', '-', '#', '@', '#', '#', '-', '-', '.', '.', '#'],
+    ['-', '-', '-', '-', '#', '-', '-', '-', '-', '-', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+    ['-', '-', '-', '-', '#', '#', '#', '#', '#', '#', '#', '-', '-', '-', '-', '-', '-', '-', '-']
+]
 
-# Convert the multiline string into a matrix
-matrix = [list(line) for line in input_string.strip().split('\n')]
+# Sprite file paths
+SPRITES = {
+    '#': ":resources:images/tiles/boxCrate_single.png",
+    '-': ":resources:images/tiles/water.png",
+    '$': ":resources:images/tiles/lockRed.png",
+    '@': ":resources:images/tiles/switchGreen.png"
+}
 
-# Output the matrix to verify
-for row in matrix:
-    print(row)
-
-print(matrix[0][4])
-print(matrix[2][5])
-print(matrix[10][18])
-
-
-class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
+class Player:
+    SPRITE_ADJUSTMENT_X = 30
+    SPRITE_ADJUSTMENT_Y = 40
+    x = 0
+    y = 0
+    sprite = None
 
     def __init__(self):
+        self.set_player_initial_position()
+        self.set_player_sprite()
 
-        # Call the parent class and set up the window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT,
-                         SCREEN_TITLE, resizable=True)
+    def set_player_initial_position(self):
+        for i in range(len(level)):
+            for j in range(len(level[0])):
+                if level[i][j] == '@':
+                    print('i = {a}'.format(a=i))
+                    print('j = {a}'.format(a=j))
+                    self.x = j
+                    self.y = len(level) - i - 1
+                    return True
+        return False
 
-        # Our TileMap Object
-        self.tile_map = None
-
-        # Our Scene Object
-        self.scene = None
-
-        # Separate variable that holds the player sprite
-        self.player_sprite = None
-
-        # Our physics engine
-        self.physics_engine = None
-
-        # A Camera that can be used for scrolling the screen
-        self.camera_sprites = None
-
-        # A non-scrolling camera that can be used to draw GUI elements
-        self.camera_gui = None
-
-        # Keep track of the score
-        self.score = 0
-
-        # What key is pressed down?
-        self.left_key_down = False
-        self.right_key_down = False
-
-    def setup(self):
-        """Set up the game here. Call this function to restart the game."""
-
-        # Setup the Cameras
-        self.camera_sprites = arcade.Camera(self.width, self.height)
-        self.camera_gui = arcade.Camera(self.width, self.height)
-
-        # Name of map file to load
-        map_name = ":resources:tiled_maps/map.json"
-
-        # Layer specific options are defined based on Layer names in a dictionary
-        # Doing this will make the SpriteList for the platforms layer
-        # use spatial hashing for detection.
-        layer_options = {
-            "Platforms": {
-                "use_spatial_hash": True,
-            },
-        }
-
-        # Read in the tiled map
-        self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
-
-        # Initialize Scene with our TileMap, this will automatically add all layers
-        # from the map as SpriteLists in the scene in the proper order.
-        self.scene = arcade.Scene.from_tilemap(self.tile_map)
-
-        # Set the background color
-        #if self.tile_map.background_color:
-        #    arcade.set_background_color(self.tile_map.background_color)
-
-        # Keep track of the score
-        self.score = 0
-
-        # Set up the player, specifically placing it at these coordinates.
-        src = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
-        self.player_sprite = arcade.Sprite(src, CHARACTER_SCALING)
-        self.player_sprite.center_x = 128
-        self.player_sprite.center_y = 128
-        self.scene.add_sprite("Player", self.player_sprite)
-
-        # --- Other stuff
-        # Create the 'physics engine'
-        self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Platforms"]
+    def set_player_sprite(self):
+        self.sprite = arcade.Sprite(
+            ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png",
+            scale=0.5
         )
+        self.update_player_position()
 
-    def on_draw(self):
-        """Render the screen."""
+    def move_up(self):
+        self.y += 1
+        self.update_player_position()
 
-        # Clear the screen to the background color
-        self.clear()
+    def move_down(self):
+        self.y -= 1
+        self.update_player_position()
 
-        # Activate the game camera
-        self.camera_sprites.use()
+    def move_left(self):
+        self.x -= 1
+        self.update_player_position()
 
-        # Draw our Scene
-        # Note, if you a want pixelated look, add pixelated=True to the parameters
-        self.scene.draw()
+    def move_right(self):
+        self.x += 1
+        self.update_player_position()
 
-        # Activate the GUI camera before drawing GUI elements
-        self.camera_gui.use()
+    def update_player_position(self):
+        self.sprite.center_x = self.x * SPRITE_SIZE + self.SPRITE_ADJUSTMENT_X
+        self.sprite.center_y = self.y * SPRITE_SIZE + self.SPRITE_ADJUSTMENT_Y
 
-        # Draw our score on the screen, scrolling it with the viewport
-        score_text = f"Score: {self.score}"
-        arcade.draw_text(score_text,
-                         start_x=10,
-                         start_y=10,
-                         color=arcade.csscolor.WHITE,
-                         font_size=18)
 
-    def update_player_speed(self):
+class SokobanLevel(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.BLACK)
 
-        # Calculate speed based on the keys pressed
-        self.player_sprite.change_x = 0
+        self.player = Player()
 
-        if self.left_key_down and not self.right_key_down:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
-        elif self.right_key_down and not self.left_key_down:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+        self.sprites = arcade.SpriteList()
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
+        if key in [arcade.key.W, arcade.key.UP]:
+            self.player.move_up()
+        elif key in [arcade.key.S, arcade.key.DOWN]:
+            self.player.move_down()
+        elif key in [arcade.key.A, arcade.key.LEFT]:
+            self.player.move_left()
+        elif key in [arcade.key.D, arcade.key.RIGHT]:
+            self.player.move_right()
 
-        # Jump
-        if key == arcade.key.UP or key == arcade.key.W:
-            if self.physics_engine.can_jump():
-                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+    def setup(self):
+        # Load and position the sprites
+        for row_index, row in enumerate(level):
+            for col_index, item in enumerate(row):
+                if item in SPRITES:
+                    sprite = arcade.Sprite(SPRITES[item], scale=0.5)
+                    sprite.center_x = col_index * SPRITE_SIZE + SPRITE_SIZE / 2
+                    sprite.center_y = (len(level) - row_index - 1) * SPRITE_SIZE + SPRITE_SIZE / 2
+                    self.sprites.append(sprite)
 
-        # Left
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.left_key_down = True
-            self.update_player_speed()
 
-        # Right
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.right_key_down = True
-            self.update_player_speed()
-
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key."""
-        if key == arcade.key.LEFT or key == arcade.key.A:
-            self.left_key_down = False
-            self.update_player_speed()
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.right_key_down = False
-            self.update_player_speed()
-
-    def center_camera_to_player(self):
-        # Find where player is, then calculate lower left corner from that
-        screen_center_x = self.player_sprite.center_x - (self.camera_sprites.viewport_width / 2)
-        screen_center_y = self.player_sprite.center_y - (self.camera_sprites.viewport_height / 2)
-
-        # Set some limits on how far we scroll
-        if screen_center_x < 0:
-            screen_center_x = 0
-        if screen_center_y < 0:
-            screen_center_y = 0
-
-        # Here's our center, move to it
-        player_centered = screen_center_x, screen_center_y
-        self.camera_sprites.move_to(player_centered)
+    def on_draw(self):
+        arcade.start_render()
+        self.sprites.draw()
+        self.player.sprite.draw()
 
     def on_update(self, delta_time):
-        """Movement and game logic"""
-
-        # Move the player with the physics engine
-        self.physics_engine.update()
-
-        # See if we hit any coins
-        coin_hit_list = arcade.check_for_collision_with_list(
-            self.player_sprite, self.scene["Coins"]
-        )
-
-        # Loop through each coin we hit (if any) and remove it
-        for coin in coin_hit_list:
-            # Remove the coin
-            coin.remove_from_sprite_lists()
-            # Add one to the score
-            self.score += 1
-
-        # Position the camera
-        self.center_camera_to_player()
-
-    def on_resize(self, width, height):
-        """ Resize window """
-        self.camera_sprites.resize(int(width), int(height))
-        self.camera_gui.resize(int(width), int(height))
-
+        self.player.sprite.update()
 
 def main():
-    """Main function"""
-    window = MyGame()
+    window = SokobanLevel()
     window.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
