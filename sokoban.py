601c384bba2b3b5069857f274000b749b1b7fd67
Tyago Teoi <tyago.teoi@usp.br>
2024-09-13 18:34:57 -0300

    Modulate player class

--- sokoban.py
+++ sokoban.py
@@ -1,5 +1,5 @@
 import arcade
-import scripts.sokoplayer
+from scripts.sokoplayer import Player
 
 # Define constants for the screen
 SCREEN_WIDTH = 1280
@@ -37,62 +37,13 @@ SPRITES = {
     '@': ":resources:images/tiles/switchGreen.png"
 }
 
-class Player:
-    SPRITE_ADJUSTMENT_X = 30
-    SPRITE_ADJUSTMENT_Y = 40
-    x = 0
-    y = 0
-    sprite = None
-
-    def __init__(self):
-        self.set_player_initial_position()
-        self.set_player_sprite()
-
-    def set_player_initial_position(self):
-        for i in range(len(level)):
-            for j in range(len(level[0])):
-                if level[i][j] == '@':
-                    print('i = {a}'.format(a=i))
-                    print('j = {a}'.format(a=j))
-                    self.x = j
-                    self.y = len(level) - i - 1
-                    return True
-        return False
-
-    def set_player_sprite(self):
-        self.sprite = arcade.Sprite(
-            ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png",
-            scale=0.5
-        )
-        self.update_player_position()
-
-    def move_up(self):
-        self.y += 1
-        self.update_player_position()
-
-    def move_down(self):
-        self.y -= 1
-        self.update_player_position()
-
-    def move_left(self):
-        self.x -= 1
-        self.update_player_position()
-
-    def move_right(self):
-        self.x += 1
-        self.update_player_position()
-
-    def update_player_position(self):
-        self.sprite.center_x = self.x * SPRITE_SIZE + self.SPRITE_ADJUSTMENT_X
-        self.sprite.center_y = self.y * SPRITE_SIZE + self.SPRITE_ADJUSTMENT_Y
-
 
 class SokobanLevel(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.BLACK)
 
-        self.player = Player()
+        self.player = Player(level, SPRITE_SIZE)
 
         self.sprites = arcade.SpriteList()
 
