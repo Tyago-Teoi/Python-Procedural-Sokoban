8cf6b9ddbecf71817c0a7bd64c333f1cabdffdd2
Tyago Teoi <tyago.teoi@usp.br>
2024-11-09 22:54:06 -0300

    Refactor procedural_level_generator names

--- scripts/procedural_level_generator.py
+++ scripts/procedural_level_generator.py
@@ -2,14 +2,15 @@ from scripts.artificial_agents.destructor_agent import DestructorAgent
 from scripts.artificial_agents.constructor_agent import ConstructorAgent
 from scripts.artificial_agents.chance import Chance
 
-MAX_ITERATIONS = 1024
+MAX_ITERATIONS = 256
 MAX_AGENTS = 4
 INITIAL_AGENTS_NUMBER = 2
 
 class LevelGenerator:
     width, height = 0, 0
+    environment = None
     difficulty = 0
-    matrix = None
+    level = None
     agents = []
     player_params = None
 
@@ -18,27 +19,27 @@ class LevelGenerator:
         self.height = height
         self.difficulty = difficulty
         self.player_params = player_params
-        self.matrix = self.allocate_level_matrix()
+        self.level = self.allocate_level()
 
     def generate_level(self):
         self.insert_level_border()
         self.initiate_agents()
         self.start_agents_generation()
-        return self.matrix
+        return self.level
 
     def insert_level_border(self):
         for i in range(self.height + 2):
-            self.matrix[i][0] = '#'
-            self.matrix[i][self.width+1] = '#'
+            self.level[i][0] = '#'
+            self.level[i][self.width+1] = '#'
         for j in range(self.width + 2):
-            self.matrix[0][j] = '#'
-            self.matrix[self.height+1][j] = '#'
+            self.level[0][j] = '#'
+            self.level[self.height+1][j] = '#'
 
     def initiate_agents(self):
         chance = Chance()
         chance.default_chance()
-        destructor_agent = DestructorAgent(chance, self.player_params, self.matrix, .5, .5)
-        constructor_agent = ConstructorAgent(chance, self.player_params, self.matrix, .6, .4)
+        destructor_agent = DestructorAgent(chance, self.player_params, self.level, .5, .5)
+        constructor_agent = ConstructorAgent(chance, self.player_params, self.level, .6, .4)
         self.agents.append(destructor_agent)
         self.agents.append(constructor_agent)
 
@@ -51,15 +52,15 @@ class LevelGenerator:
             self.print()
             print()
 
-    def allocate_level_matrix(self):
+    def allocate_level(self):
         return [['-' for _ in range(self.width + 2)] for _ in range(self.height + 2)]
 
     def print(self):
         for i in range(self.height + 2):
             for j in range(self.width + 2):
-                print(self.matrix[i][j], end = " ")
+                print(self.level[i][j], end = " ")
             print()
 
-test = LevelGenerator(6, 6, 1, None)
+test = LevelGenerator(8, 4, 1, None)
 test.generate_level()
 test.print()
