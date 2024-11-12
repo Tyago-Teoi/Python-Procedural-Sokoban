bb6ecc3f9568bd7d05d7959c1e60bd8e00452803
Tyago Teoi <tyago.teoi@usp.br>
2024-11-12 11:23:41 -0300

    Add random chance method in chance script

--- scripts/artificial_agents/chance.py
+++ scripts/artificial_agents/chance.py
@@ -1,3 +1,5 @@
+import random
+
 class Chance:
     walk = .3
     stop = .15
@@ -7,7 +9,18 @@ class Chance:
     interact = .15
 
     def __init__(self):
-        pass
+        self.get_random_chance()
+
+    def get_random_chance(self):
+        values = [random.random() for _ in range(6)]
+        total = sum(values)
+
+        self.walk = values[0] / total
+        self.stop = values[1] / total
+        self.turn_left = values[2] / total
+        self.turn_right = values[3] / total
+        self.turn_back = values[4] / total
+        self.interact = values[5] / total
 
     def update_chance(self, c_w, c_s, c_l, c_r, c_b, c_i):
         self.walk = c_w
@@ -20,4 +33,14 @@ class Chance:
     def default_chance(self):
         return self.update_chance(.3, .15, .175, .175, .05, .15)
 
+    def print(self):
+        print('walk: {a}'.format(a=self.walk))
+        print('stop: {a}'.format(a=self.stop))
+        print('turn_left: {a}'.format(a=self.turn_left))
+        print('turn_right: {a}'.format(a=self.turn_right))
+        print('turn_back: {a}'.format(a=self.turn_back))
+        print('interact: {a}'.format(a=self.interact))
+        print()
 
+#c = Chance()
+#c.print()
